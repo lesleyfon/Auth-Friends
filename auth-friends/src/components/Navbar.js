467044da9ec4511161cfc76691003d9d6e131bee
lesley <lesleybanadzem2017@gmail.com>
2019-11-01 13:44:00 -0700

    Styled Header

--- auth-friends/src/components/Navbar.js
+++ auth-friends/src/components/Navbar.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
+import styled from 'styled-components';
 
 function Navbar() {
-    
+
     return (
-        <nav>
+        <Nav>
             <Link to ='/'> home </Link>
             <Link to ='/signin'> Sign in</Link>
             <Link to ='/account'> Account </Link>
             <Link to ='/signin'> Sign Out  </Link>
-        </nav>
+        </Nav>
     )
 }
 
 export default Navbar
+
+const Nav = styled.nav`
+        width: 760px;
+        display: flex;
+        justify-content: space-evenly;
+        margin: 0 auto;
+        padding: 20px;
+        /* text-decoration: none; */
+        font-weight: bolder;
+        text-transform: capitalize;
+        font-size: 1.2rem;
+        a{
+            text-decoration: none;
+            color: lightcoral
+        }
+`;
